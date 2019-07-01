6d8a7c02bf426edde3ca6e03428ad7f2ffdeefca
Marcin Bigos <marcin.bigos99@gmail.com>
2019-07-01 23:42:50 +0200

    add test moods controller and cords settings

---
+++ src/MyMind.Web/Areas/Moods/Controllers/MoodsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyMind.Web.Areas.Moods.Controllers
+{
+    public class MoodsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Test(string q)
+        {
+            return Json(q);
+        }
+    }
+}
--- src/MyMind.Web/Startup.cs
+++ src/MyMind.Web/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Patterns;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,13 @@ namespace MyMind.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+
             services.AddControllers();
         }
 
@@ -36,6 +45,8 @@ namespace MyMind.Web
             }
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
