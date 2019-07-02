338726e138bd3f9eab8206349d327f3d08795a06
Marcin Bigos <marcin.bigos99@gmail.com>
2019-07-02 03:00:46 +0200

    add test data

--- src/MyMind.Web/Controllers/TestController.cs
+++ src/MyMind.Web/Controllers/TestController.cs
@@ -4,13 +4,71 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyMind.Web.Areas;
+using MyMind.Web.Database;
+using MyMind.Web.Domain.Emotions;
 
 namespace MyMind.Web.Controllers
 {
     public class TestController : BaseApiController
     {
+        private readonly ISessionFactory _sessionFactory;
+
+        public TestController(ISessionFactory sessionFactory)
+        {
+            _sessionFactory = sessionFactory;
+        }
+
+        [HttpGet]
         public IActionResult DatabaseInit()
         {
+            var emotions = new List<Emotion>
+            {
+                new Emotion("Playful", EmotionType.Happy),
+                new Emotion("Interested", EmotionType.Happy),
+                new Emotion("Proud", EmotionType.Happy),
+                new Emotion("Powerful", EmotionType.Happy),
+                new Emotion("Peaceful", EmotionType.Happy),
+                new Emotion("Optimistic", EmotionType.Happy),
+
+                new Emotion("Lonely", EmotionType.Sad),
+                new Emotion("Guilty", EmotionType.Sad),
+                new Emotion("Depressed", EmotionType.Sad),
+                new Emotion("Disappointed", EmotionType.Sad),
+
+                new Emotion("Awful", EmotionType.Disgusted),
+                new Emotion("disapproving", EmotionType.Disgusted),
+
+                new Emotion("Frustrated", EmotionType.Angry),
+                new Emotion("Aggressive", EmotionType.Angry),
+                new Emotion("Mad", EmotionType.Angry),
+                new Emotion("Humiliated", EmotionType.Angry),
+
+                new Emotion("Insecure", EmotionType.Fearful),
+                new Emotion("Anxious", EmotionType.Fearful),
+                new Emotion("Scared", EmotionType.Fearful),
+                new Emotion("Weak", EmotionType.Fearful),
+                new Emotion("Rejected", EmotionType.Fearful),
+
+                new Emotion("Bored", EmotionType.Bad),
+                new Emotion("Stressed", EmotionType.Bad),
+                new Emotion("Tired", EmotionType.Bad),
+
+                new Emotion("Confused", EmotionType.Surprised),
+                new Emotion("Amazed", EmotionType.Surprised),
+                new Emotion("Excited", EmotionType.Surprised),
+            };
+
+            using (var session = this._sessionFactory.Create())
+            {
+                if(session.Get<Emotion>().Any())
+                {
+                    return Json("Cannot init not empty database. Database must be empty.");
+                } 
+
+                emotions.ForEach(session.Add);
+            }
+
+
             return Json("Created");
         }
     }
