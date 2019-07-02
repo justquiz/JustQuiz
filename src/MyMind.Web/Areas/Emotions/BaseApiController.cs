2adbd7b55d94803a6f2f076410d3d52041d9ed3e
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-07-03 01:33:05 +0200

    architecture

---
+++ src/MyMind.Web/Areas/Emotions/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMind.Web.Areas.Emotions
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public abstract class BaseApiController : Controller
+    {
+
+    }
+}
---
+++ src/MyMind.Web/Areas/Emotions/Controllers/EmotionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMind.Web.Areas.Emotions
+{
+    public class EmotionsController : BaseApiController
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
