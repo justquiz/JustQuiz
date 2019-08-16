18896132bc4fd664ae09acf64db4ad4c80d3842d
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-08-16 22:45:35 +0200

    get test json with emotions from backend

--- src/MyMind.Web/ClientApp/src/app/app.component.ts
+++ src/MyMind.Web/ClientApp/src/app/app.component.ts
@@ -10,12 +10,13 @@ export class AppComponent implements OnInit {
 
   constructor(private _httpService: HttpClient) { }
 
-  emotions: string[] = [];
+  emotions: object[] = [];
+  error: any;
 
   ngOnInit(): void {
-    this._httpService.get('localhost:22077/api/Emotions/GetEmotions').subscribe(values => {
-      this.emotions = JSON.parse(values as string);
-    });
+    this._httpService.get('http://localhost:22077/api/Emotions/GetEmotions')
+      .subscribe((data: any[]) => this.emotions = [...data], // success path
+                  error => this.error = error); // error path
   }
 
   title = 'MyMind';
