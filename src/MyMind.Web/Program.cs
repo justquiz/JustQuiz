dd3e4b7efd50e40d44085c1693fb4055497494dc
Mateusz Strycharz <strycharz.mateusz0@gmail.com>
2019-08-22 17:01:38 +0200

    set backend port to 5000

--- src/MyMind.Web/Program.cs
+++ src/MyMind.Web/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -19,6 +20,11 @@ namespace MyMind.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureKestrel((context, options) =>
+                {
+                    options.Listen(IPAddress.Loopback, 5000);
+                })
+                .PreferHostingUrls(false);
     }
 }
